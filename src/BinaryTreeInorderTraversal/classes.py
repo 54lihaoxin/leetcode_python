cca329d8115689efbeb7be7480af8e17dedda909
54lihaoxin <54lihaoxin@gmail.com>
2014-08-14 22:43:46 -0700

    add Binary Tree Inorder Traversal

---
+++ src/BinaryTreeInorderTraversal/classes.py
@@ -0,0 +1,12 @@
+
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        
+    def __repr__(self):
+        return '[{0}]'.format(self.val)
+    
---
+++ src/BinaryTreeInorderTraversal/solution.py
@@ -0,0 +1,32 @@
+
+
+debug = True
+debug = False
+
+
+from classes import TreeNode   # hxl: comment out this line for submission
+
+
+class Solution:
+
+    # @param root, a tree node
+    # @return a list of integers
+    def inorderTraversal(self, root):
+        
+        if debug: print root
+        
+        if root == None:
+            return []
+        
+        r = []
+        
+        if root.left != None:
+            r += self.inorderTraversal(root.left)
+            
+        r += [root.val]
+        
+        if root.right != None:
+            r += self.inorderTraversal(root.right)
+        
+        return r
+        
