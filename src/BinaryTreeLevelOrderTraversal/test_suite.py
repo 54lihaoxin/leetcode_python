f78f52cf0cd30f7dd3d919e7c7cbbda5c3b6245a
54lihaoxin <54lihaoxin@gmail.com>
2014-08-15 00:21:49 -0700

    Binary Tree Level Order Traversal

---
+++ src/BinaryTreeLevelOrderTraversal/test_suite.py
@@ -0,0 +1,89 @@
+
+
+import sys
+from solution import Solution
+from classes import TreeNode
+
+
+class TestSuite:
+    
+    def run(self):
+#         self.test001()
+#         self.test002()
+        self.test003()
+#         self.test004()
+#         self.test005()
+#         self.test006()
+#         self.test007()
+
+    def test001(self):
+        
+        print "test 001"
+        
+        # hxl: see http://en.wikipedia.org/wiki/Tree_traversal
+        
+        nA = TreeNode('A')
+        nB = TreeNode('B')
+        nC = TreeNode('C')
+        nD = TreeNode('D')
+        nE = TreeNode('E')
+        nF = TreeNode('F')  # hxl: root
+        nG = TreeNode('G')
+        nH = TreeNode('H')
+        nI = TreeNode('I')
+        nF.left = nB
+        nF.right = nG
+        nB.left = nA
+        nB.right = nD
+        nG.right = nI
+        nD.left = nC
+        nD.right = nE
+        nI.left = nH
+        
+        r = Solution().levelOrder(nF)
+        
+        print "  input:\t", nF
+        print "  output:\t", r
+        print
+    
+    def test002(self):
+        
+        print "test 002"
+        
+        nA = TreeNode('A')
+        
+        r = Solution().levelOrder(nA)
+        
+        print "  input:\t", nA
+        print "  output:\t", r
+        print
+
+    def test003(self):
+        
+        print "test 003"
+        
+        # hxl: see http://en.wikipedia.org/wiki/Tree_traversal
+        
+        nA = TreeNode('A')
+        nB = TreeNode('B')
+        nC = TreeNode('C')
+        nD = TreeNode('D')
+        nE = TreeNode('E')
+
+        nA.left = nB
+        nA.right = nC
+        nB.left = nD
+        nC.right = nE
+        
+        r = Solution().levelOrder(nA)
+        
+        print "  input:\t", nA
+        print "  output:\t", r
+        print
+        
+def main(argv):
+    TestSuite().run()
+
+if __name__ == '__main__':
+    main(sys.argv)
+    
