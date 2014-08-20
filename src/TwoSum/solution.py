de302ef6c2cba41932967365a2ddcf9450dc3c7a
54lihaoxin <54lihaoxin@gmail.com>
2014-08-20 01:25:59 -0700

    add zero-base solution to Two Sum

--- src/TwoSum/solution.py
+++ src/TwoSum/solution.py
@@ -9,13 +9,18 @@ debug = False
 
 class Solution:
     
-    # @return a tuple, (index1, index2)
-    def twoSum(self, num, target):
+    # @return a tuple, (index1, index2); index starts with 0
+    def twoSumZeroBased(self, num, target):
         d = {}
         for i in range(0, len(num)):
-            indexA = target - num[i]
-            if d.has_key(num[i]):
-                return (d[num[i]], i + 1)   # hxl: assume there is always a match
+            complement = target - num[i]
+            if d.has_key(complement):
+                return (d[complement], i)   # hxl: assume there is always a match
             else:
-                d[target - num[i]] = i + 1
-        
+                d[num[i]] = i
+    
+    # @return a tuple, (index1, index2); index starts with 1
+    def twoSum(self, num, target):
+        r = self.twoSumZeroBased(num, target)
+        return (r[0] + 1, r[1] + 1)
+    
