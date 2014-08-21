e6528598bb9673682b654c393226eaabdb0b72d3
Haoxin Li <54lihaoxin@gmail.com>
2014-08-20 19:56:44 -0700

    add Insert Interval

---
+++ src/InsertInterval/solution.py
@@ -0,0 +1,56 @@
+
+
+debug = True
+debug = False
+
+
+from classes import Interval   # hxl: comment out this line for submission
+
+# hxl: Note: If the intervals line up on a loop (like 1 ~ 12 on the clock)...
+#            Simple break down the cross-origin intervals into two halves upon origin,
+#            and then merge them back at the end.
+
+class Solution:
+    
+    # @param intervals, a list of sorted Intervals
+    # @param newInterval, a Interval
+    # @return a list of Interval
+    def insert(self, intervals, newInterval):
+        
+        if len(intervals) == 0:
+            return [newInterval]
+        
+        r = []
+        overlapWithNewInterval = Interval(newInterval.start, newInterval.end)
+        didAddNewInterval = False
+        
+        for i in intervals:
+            if (i.start < overlapWithNewInterval.start
+                  and i.end < overlapWithNewInterval.start):
+                r.append(i)
+            elif (i.start < overlapWithNewInterval.start
+                  and i.end >= overlapWithNewInterval.start
+                  and i.end <= overlapWithNewInterval.end):
+                overlapWithNewInterval.start = i.start 
+            elif (i.start < overlapWithNewInterval.start
+                  and i.end > overlapWithNewInterval.end):
+                return intervals
+            elif (i.start >= overlapWithNewInterval.start
+                  and i.end <= overlapWithNewInterval.end):
+                pass
+            elif (i.start >= overlapWithNewInterval.start
+                  and i.start <= overlapWithNewInterval.end
+                  and i.end >= overlapWithNewInterval.end):
+                overlapWithNewInterval.end = i.end
+            else:   # hxl: i is after overlapWithNewInterval
+                if didAddNewInterval == False:
+                    r.append(overlapWithNewInterval)
+                    didAddNewInterval = True
+                r.append(i)
+        
+        # hxl: It's possible that the new interval is not added yet!
+        if didAddNewInterval == False:
+            r.append(overlapWithNewInterval)
+            didAddNewInterval = True
+        return r
+    
---
+++ src/InsertInterval/test_suite.py
@@ -0,0 +1,78 @@
+
+
+import sys
+from solution import Solution
+from classes import Interval
+
+
+class TestSuite:
+    
+    def run(self):
+        self.test000()
+        self.test001()
+        self.test002()
+        self.test003()
+#         self.test004()
+
+    def test000(self):
+        
+        print 'test 000\n'
+        
+        a = [Interval(1, 3),
+             Interval(6, 9)]
+        b = Interval(2, 5)
+        r = Solution().insert(a, b)
+        print '  input:\t', a, b
+        print '  expect:\t', [[1,5],[6,9]] 
+        print '  output:\t', r
+        print
+
+    def test001(self):
+        
+        print 'test 001\n'
+        
+        a = [Interval(1, 2),
+             Interval(3, 5),
+             Interval(6, 7),
+             Interval(8, 10),
+             Interval(12, 16)]
+        b = Interval(4, 9)
+        r = Solution().insert(a, b)
+        print '  input:\t', a, b
+        print '  expect:\t', [[1,2],[3,10],[12,16]]
+        print '  output:\t', r
+        print
+
+    def test002(self):
+        
+        print 'test 002\n'
+        
+        a = [Interval(2, 5),
+             Interval(3, 7),
+             Interval(8, 9)]
+        b = Interval(0, 1)
+        r = Solution().insert(a, b)
+        print '  input:\t', a, b
+        print '  expect:\t', [[0,1],[2,5],[3,7],[8,9]]
+        print '  output:\t', r
+        print
+
+    def test003(self):
+        
+        print 'test 003\n'
+        
+        a = [Interval(1, 5)]
+        b = Interval(2, 7)
+        r = Solution().insert(a, b)
+        print '  input:\t', a, b
+        print '  expect:\t', [[1,7]]
+        print '  output:\t', r
+        print
+
+        
+def main(argv):
+    TestSuite().run()
+
+if __name__ == '__main__':
+    main(sys.argv)
+    
