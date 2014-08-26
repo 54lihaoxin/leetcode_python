602be2db377a96faf6902580bb96f194228443a6
54lihaoxin <54lihaoxin@gmail.com>
2014-08-26 01:22:38 -0700

    fix a bug that didn't consider overflow correctly

--- src/AddTwoNumbers/solution.py
+++ src/AddTwoNumbers/solution.py
@@ -21,25 +21,28 @@ class Solution:
         while p1 != None or p2 != None:
             if debug: print p1.val, p2.val, overflow
             if overflow:
-                n = (p1.val + p2.val + 1) % 10
+                n = p1.val + p2.val + 1
+                overflow = n >= 10  # hxl: Don't put this line above the assignment of n!
             else:
-                n = (p1.val + p2.val) % 10
-            overflow = (p1.val + p2.val) >= 10  # hxl: Don't put this line above the assignment of n!
+                n = p1.val + p2.val
+                overflow = n >= 10  # hxl: Don't put this line above the assignment of n!
+                
             p1 = p1.next
             p2 = p2.next
             
             if r == None:
-                cur = ListNode(n)
+                cur = ListNode(n % 10)
                 r = cur
             else:
-                cur.next = ListNode(n)
+                cur.next = ListNode(n % 10)
                 cur = cur.next
                 
             if p1 == None:  # hxl: consume p2 if p1 is consumed
                 while p2 != None:
                     if overflow:
-                        cur.next = ListNode(p2.val + 1)
-                        overflow = False
+                        n = p2.val + 1
+                        cur.next = ListNode(n % 10)
+                        overflow = n >= 10
                     else:
                         cur.next = ListNode(p2.val)
                     cur = cur.next
@@ -48,12 +51,16 @@ class Solution:
             if p2 == None:  # hxl: consume p1 if p2 is consumed
                 while p1 != None:
                     if overflow:
-                        cur.next = ListNode(p1.val + 1)
-                        overflow = False
+                        n = p1.val + 1
+                        cur.next = ListNode(n % 10)
+                        overflow = n >= 10
                     else:
                         cur.next = ListNode(p1.val)
                     cur = cur.next
                     p1 = p1.next
+        
+        if overflow:    # hxl: It's possible that the overflow 1 hasn't been added!
+            cur.next = ListNode(1)
             
         return r
         
--- src/AddTwoNumbers/test_suite.py
+++ src/AddTwoNumbers/test_suite.py
@@ -14,6 +14,8 @@ class TestSuite:
         self.test002()
         self.test003()
         self.test004()
+        self.test005()
+        self.test006()
 
     def test000(self):
         
@@ -72,6 +74,30 @@ class TestSuite:
         r = Solution().addTwoNumbers(l1, l2)
         print '  input:\t', l1, l2
         print '  expect:\t', listFromList([7,0,2,2,1])
+        print '  output:\t', r
+        print
+
+    def test005(self):
+        
+        print 'test 005\n'
+        
+        l1 = listFromList([5])
+        l2 = listFromList([5])
+        r = Solution().addTwoNumbers(l1, l2)
+        print '  input:\t', l1, l2
+        print '  expect:\t', listFromList([0,1])
+        print '  output:\t', r
+        print
+
+    def test006(self):
+        
+        print 'test 006\n'
+        
+        l1 = listFromList([9,9])
+        l2 = listFromList([1])
+        r = Solution().addTwoNumbers(l1, l2)
+        print '  input:\t', l1, l2
+        print '  expect:\t', listFromList([0,0,1])
         print '  output:\t', r
         print
         
