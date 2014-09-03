8d8ef447b4d20ef1b5062717563c4a12be7e2db0
54lihaoxin <54lihaoxin@gmail.com>
2014-09-03 08:39:59 -0700

    add Generate Parentheses

---
+++ src/GenerateParentheses/solution.py
@@ -0,0 +1,34 @@
+
+
+debug = True
+debug = False
+
+
+# from ? import ?   # hxl: comment out this line for submission
+ 
+ 
+class Solution:
+    
+    def __init__(self):
+        self.result = []
+    
+    # @param an integer
+    # @return a list of string
+    def generateParenthesis(self, n):
+        
+        if n == 0:
+            return []
+        
+        self.depthFirstSearch(n, 1, 0, '(')
+        
+        return self.result
+    
+    def depthFirstSearch(self, n, nOpen, nClose, curString):
+        if nOpen >= nClose: # hxl: existing closing parenthesis cannot be more than the opening ones
+            if n > nOpen:
+                self.depthFirstSearch(n, nOpen + 1, nClose, curString + '(')
+            if n > nClose:
+                self.depthFirstSearch(n, nOpen, nClose + 1, curString + ')')
+            if n == nOpen and n == nClose:
+                self.result.append(curString)
+        
