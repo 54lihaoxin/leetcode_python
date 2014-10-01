1bb741a8865d4bdc6f3382d4e2903070c85947e9
54lihaoxin <54lihaoxin@gmail.com>
2014-09-30 22:59:29 -0700

    add __repr__

--- src/Classes/RandomListNode.py
+++ src/Classes/RandomListNode.py
@@ -1,8 +1,12 @@
 
 # Definition for singly-linked list with a random pointer.
 class RandomListNode:
+    
     def __init__(self, x):
         self.label = x
         self.next = None
         self.random = None
-        
+    
+    def __repr__(self):
+        return '[{0}]'.format(self.label)
+    
