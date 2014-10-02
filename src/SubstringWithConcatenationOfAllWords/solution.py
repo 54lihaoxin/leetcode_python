df9455ef1218181f72e76e38057f7a751c40c1ac
Haoxin Li <54lihaoxin@gmail.com>
2014-10-01 17:25:40 -0700

    add a solution from http://bookshadow.com/weblog/2014/04/24/leetcode-substring-concatenation-all-words/

--- src/SubstringWithConcatenationOfAllWords/solution.py
+++ src/SubstringWithConcatenationOfAllWords/solution.py
@@ -19,53 +19,81 @@ debug = False
 
 class Solution:
     
+    
+    # hxl: this solution is from http://bookshadow.com/weblog/2014/04/24/leetcode-substring-concatenation-all-words/
+    
+    
     # @param S, a string
     # @param L, a list of string
     # @return a list of integer
     def findSubstring(self, S, L):
-        
+        lenL = len(L)
         wordLen = len(L[0])
-        if wordLen > len(S):
-            return []
-        
-        matchDict = {}
-        matchList = []
-        for i in range(len(S) - wordLen + 1): 
-            curWord = S[i:i + wordLen]
-            if debug: print curWord
-            if curWord in L:
-                if curWord not in matchDict:
-                    matchDict[curWord] = []
-                matchDict[curWord].append(i)
-                matchList.append(i)
-        return self.listOfValidSubstringStartIndex(S, L, matchList)
-        
-    def listOfValidSubstringStartIndex(self, S, L, candidates):
-        
-        if debug: print candidates
-        if len(candidates) == 1:
-            return candidates
-        
-        r = []
-        wordLen = len(L[0])
-        
-        for i in range(len(candidates) - len(L) + 1):
-            curWord = S[candidates[i]:candidates[i] + wordLen]
-            remaining = list(L)
-            remaining.remove(curWord)
-            j = 1
-            while j < len(L) and j < len(candidates):
-                nextIndex = candidates[i] + j * wordLen
-                if debug: print i, j, nextIndex
-                if nextIndex in candidates:
-                    curWord = S[nextIndex:nextIndex + wordLen]
-                    if curWord in remaining:
-                        remaining.remove(curWord)
+        tree, numDistinctWords = self.buildTree(L)
+        strlen = len(S)
+        arr = list()
+        for i in range(strlen - wordLen + 1):
+            arr.append(self.valTree(S, i, wordLen, tree))
+        result = []
+        dp = list()
+        for i in range(strlen - wordLen + 1):
+            wordDict = None
+            wordSet = set()
+            accm = 0
+            if arr[i][0] > 0:   # hxl: 'id' is associated with a word in list
+                idx = i - wordLen * (lenL - 1)
+                if i < wordLen:
+                    wordDict = {arr[i][0]:1}
+                    if arr[i][1] == 1:  # hxl: number of repetition is 1
+                        wordSet.add(arr[i][0])
+                    if len(wordSet) == numDistinctWords:
+                        result.append(i)
                 else:
-                    break
-                j += 1
-            if len(remaining) == 0:
-                r.append(candidates[i])
-                
-        return r
-        
+                    wordDict,wordSet,accm = dp[i - wordLen]
+                    if wordDict is None:
+                        wordDict = dict()
+                    accm += 1
+                    num = wordDict.get(arr[i][0],0)
+                    wordDict[arr[i][0]] = num + 1
+                    if num + 1 >= arr[i][1]:
+                        wordSet.add(arr[i][0])
+                    if idx >= 0:
+                        if len(wordSet) == numDistinctWords:
+                            result.append(idx)
+                        if accm >= lenL - 1:
+                            tmp = arr[idx]
+                            if tmp[0] > 0:
+                                num = wordDict.get(tmp[0],0)
+                            if num - 1 < tmp[1] and tmp[0] in wordSet:
+                                wordSet.remove(tmp[0])
+                            if num >= 1:
+                                wordDict[tmp[0]] = num - 1
+            dp.append((wordDict,wordSet,accm))
+        return result
+    
+    def buildTree(self, L):
+        tree = dict()
+        numDistinctWords = 0
+        for word in L:
+            node = tree
+            for letter in word:
+                child = node.get(letter)
+                if child is None:
+                    child = dict()
+                    node[letter] = child
+                node = child
+            repetition = node.get('repetition',0)
+            if repetition == 0:
+                numDistinctWords += 1
+                node['id'] = numDistinctWords
+            node['repetition'] = repetition + 1
+        return (tree, numDistinctWords)
+    
+    def valTree(self, word, start, wordLen, tree):
+        node = tree
+        for i in range(start, start + wordLen):
+            if node.get(word[i]) is None:
+                return (0, 0)
+            node = node[word[i]]
+        return (node['id'], node['repetition'])
+    
